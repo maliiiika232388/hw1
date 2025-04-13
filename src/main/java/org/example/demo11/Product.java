aa4389acccbc7789ff660710865ec1b88740f316
maliiiika232388 <malikaraiymm@gmail.com>
2025-04-13 17:07:55 +0600

    Update Product.java

--- src/main/java/org/example/demo11/Product.java
+++ src/main/java/org/example/demo11/Product.java
@@ -1,1 +1,20 @@
-package org.example.demo11;public class Product {    private String name;    private double price;    private int quantity;    public Product(String name, double price, int quantity) {        this.name = name;        this.price = price;        this.quantity = quantity;    }    public String getName() {        return name;    }    public double getPrice() {        return price;    }    public int getQuantity() {        return quantity;    }}
+package org.example.demo11;
+public class Product {   
+  private String name;    
+  private double price;    
+  private int quantity;    
+  public Product(String name, double price, int quantity) {        
+    this.name = name;        
+    this.price = price;       
+    this.quantity = quantity;    
+  }    
+  public String getName() {        
+    return name;    
+  }    
+  public double getPrice() {       
+    return price;    
+  }    
+  public int getQuantity() {        
+    return quantity;    
+  }
+}
