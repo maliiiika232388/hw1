027196184ce09964086b6a6103975878bec80524
maliiiika232388 <malikaraiymm@gmail.com>
2025-04-13 17:06:40 +0600

    Update MonthSales.java

--- src/main/java/org/example/demo11/MonthSales.java
+++ src/main/java/org/example/demo11/MonthSales.java
@@ -1,1 +1,21 @@
-package org.example.demo11;import java.util.List;public class MonthSales {    private String month;    private List<Product> products;    public MonthSales(String month, List<Product> products) {        this.month = month;        this.products = products;    }    public String getMonth() {        return month;    }    public List<Product> getProducts() {        return products;    }    public String getName() {        return "";    }    public Object getSum() {        return null;    }}
+package org.example.demo11;
+import java.util.List;
+public class MonthSales {    
+  private String month;    
+  private List<Product> products;    
+  public MonthSales(String month, List<Product> products) {        
+    this.month = month;        
+    this.products = products;    
+  }    
+  public String getMonth() {        
+    return month;    
+  }    
+  public List<Product> getProducts() {        
+    return products;    }    
+  public String getName() {        
+    return "";    
+  }    
+  public Object getSum() {       
+    return null;    
+  }
+}
