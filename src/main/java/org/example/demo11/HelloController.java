ebe9196633818939828edd0b7a47959268862bce
maliiiika232388 <malikaraiymm@gmail.com>
2025-04-13 17:04:56 +0600

    Update HelloController.java

--- src/main/java/org/example/demo11/HelloController.java
+++ src/main/java/org/example/demo11/HelloController.java
@@ -1,1 +1,9 @@
-package org.example.demo11;import javafx.fxml.FXML;import javafx.scene.control.Label;public class HelloController {    @FXML    private Label welcomeText;    @FXML    protected void onHelloButtonClick() {        welcomeText.setText("Welcome to JavaFX Application!");    }}
+package org.example.demo11;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+public class HelloController {   
+  @FXML    private Label welcomeText;    
+  @FXML    protected void onHelloButtonClick() {       
+    welcomeText.setText("Welcome to JavaFX Application!");    
+  }
+}
