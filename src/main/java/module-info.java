00f163309b23b61f818d027e34be26c0e0e00046
maliiiika232388 <malikaraiymm@gmail.com>
2025-04-13 17:15:29 +0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,1 +1,16 @@
-module com.example.demo1 {    requires javafx.controls;    requires javafx.fxml;    requires javafx.web;    requires org.controlsfx.controls;    requires com.dlsc.formsfx;    requires net.synedra.validatorfx;    requires org.kordamp.ikonli.javafx;    requires org.kordamp.bootstrapfx.core;    requires eu.hansolo.tilesfx;    requires com.almasb.fxgl.all;    requires org.apache.poi.ooxml;    requires commons.math3;    opens org.example.demo11 to javafx.fxml;    exports org.example.demo11;}
+module com.example.demo1 {    
+  requires javafx.controls;    
+  requires javafx.fxml;    
+  requires javafx.web;    
+  requires org.controlsfx.controls;    
+  requires com.dlsc.formsfx;    
+  requires net.synedra.validatorfx;    
+  requires org.kordamp.ikonli.javafx;   
+  requires org.kordamp.bootstrapfx.core;    
+  requires eu.hansolo.tilesfx;    
+  requires com.almasb.fxgl.all;    
+  requires org.apache.poi.ooxml;    
+  requires commons.math3;    
+  opens org.example.demo11 to javafx.fxml;   
+  exports org.example.demo11;
+}
